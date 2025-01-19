aa3493d7aee6044a9511a5d8240517b41a3f01f7
Asif Abdullah Rokoni <asifabdullahrokoni@Mac.fritz.box>
2025-01-19 21:38:10 +0100

    Fix Home page navigation, add header, footer

--- client/src/pages/CustomWidgetDevelopmentPage.js
+++ client/src/pages/CustomWidgetDevelopmentPage.js
@@ -4,11 +4,14 @@ import NavbarComponent from "../components/NavBar/NavBar";
 import DueDiligence from "../components/CustomWidgetDevelopment/DueDiligence";
 import Kyc from "../components/CustomWidgetDevelopment/Kyc";
 import RotAnalysis from "../components/CustomWidgetDevelopment/RotAnalysis";
+import HeaderCreation from "../components/HeaderCreation/HeaderCreation";
+import FooterCreation from "../components/FooterCreation/FooterCreation";
 
 const NewPage = () => {
   return (
     <div>
       <NavbarComponent />
+      <HeaderCreation />
       <div
         style={{
           display: "flex", // Enable flexbox for layout
@@ -22,6 +25,7 @@ const NewPage = () => {
         <Kyc />
         <RotAnalysis />
       </div>
+      <FooterCreation />
     </div>
   );
 };
